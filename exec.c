9d1fff45fcd74ec5d51ec080d7e9ec0e01d4ce21
KennethTL <kennethtemba17@gmail.com>
2023-07-23 19:35:27 +0300

    Modify .c files to betty standard

--- exec.c
+++ exec.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * exec: - this executes the commands and environment variables
- *
+ * exec - this executes the commands and environment variables
+ * @argv: arguments
  * Return: argv
  */
 
 void exec(char **argv)
 {
-    char *cmd = NULL;
-    if (argv)
-    {
-        /* grab the command */
-        cmd = argv[0];
-        /* execute the command with the execve()*/
-        if (execve(cmd, argv, NULL) == -1)
-        {
-            perror("Error: ");
-        };
-    }
+	char *cmd = NULL;
+
+	if (argv)
+	{
+		/* grab the command */
+		cmd = argv[0];
+		/* execute the command with the execve()*/
+		if (execve(cmd, argv, NULL) == -1)
+		{
+			perror("Error: ");
+		};
+	}
 }

916d9faf188fcc7b5136ecffaf26166cc2717490
KennethTL <kennethtemba17@gmail.com>
2023-07-29 05:59:10 +0300

    revert to shell V1

---
+++ exec.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+ * execute_command - this executes the commands and environment variables
+ * @cmd: command to execute
+ * @argv: arguments
+ */
+void execute_command(char *cmd, char **argv)
+{
+	char *actual_cmd = NULL;
+
+	if (cmd)
+	{
+		/* generate the path to the command before running into execve */
+		actual_cmd = get_env(cmd);
+		if (actual_cmd != NULL)
+		{
+			/* execute the command with execve() */
+			if (execve(actual_cmd, argv, NULL) == -1)
+			{
+				perror("Error: ");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			printf("command not found: %s\n", cmd);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+/**
+ * ls_builtin - Execute the ls command (built-in)
+ */
+void ls_builtin(void)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork failed");
+		return;
+	}
+	else if (pid == 0)
+	{
+		/* Child process, execute ls command */
+		char *argv[] = {"ls", "-l", NULL};
+
+		execvp("ls", argv);
+		perror("execvp failed");
+		exit(1);
+	}
+	else
+	{
+		/* Parent process, wait for the child to finish */
+		wait(&status);
+	}
+}
---
+++ tokenize.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * get_tokens - Tokenizes the input line using spaces & newline as delimiters.
+ * @line: The input line to be tokenized.
+ * @tokens: Pointer to the array that will store the tokens.
+ * @max_tokens: Maximum number of tokens that can be stored in the array.
+ *
+ * Return: Number of tokens found.
+ */
+int get_tokens(char *line, char *tokens[], int max_tokens)
+{
+	int num_tokens = 0;
+	char *token_start = line;
+	char *current = line;
+
+	while (*current != '\0')
+	{
+		if (*current == ' ' || *current == '\n')
+		{
+			*current = '\0'; /* Null-terminate the token */
+			tokens[num_tokens] = token_start;
+			num_tokens++;
+			if (num_tokens >= max_tokens)
+				break;
+			/* Move to the next non-space character */
+			while (*current == ' ' || *current == '\n')
+				current++;
+
+			token_start = current;
+		}
+		else
+		{
+			current++;
+		}
+	}
+	/* Add the last token if there's room in the array */
+	if (num_tokens < max_tokens)
+	{
+		tokens[num_tokens] = token_start;
+		num_tokens++;
+	}
+	return (num_tokens);
+}
